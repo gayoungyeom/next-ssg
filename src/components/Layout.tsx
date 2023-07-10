9dca64e08ef96f7e44a0ae9ccbd196bfe498b1ae
gayoung <gayoungyeom@gmail.com>
2023-07-10 14:42:02 +0900

    feat: styled-components 설정 적용

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from 'react';
 import GNB from './GNB';
+import { styled } from 'styled-components';
 
 interface LayoutProps {
   children: ReactNode;
@@ -7,11 +8,17 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <>
+    <Container>
       <GNB />
       <div>{children}</div>
-    </>
+    </Container>
   );
 };
 
 export default Layout;
+
+const Container = styled.div`
+  width: 1024px;
+  margin: 0 auto;
+  border: 2px solid orange;
+`;
